6c399c34ed64cfc918571797f6b6c2961563f7b1
shashidhar <shashidhar@amagi.com>
2024-08-16 18:41:56 +0530

    aligning golang to changes and tests in https://github.com/amagioss/tc_utils/pull/4

--- go/timecode/timecode_test.go
+++ go/timecode/timecode_test.go
@@ -2,6 +2,7 @@ package timecode_test
 
 import (
 	"fmt"
+	"math"
 	"testing"
 
 	"github.com/orgs/amagioss/tc_utils/go/timecode"
@@ -174,36 +175,115 @@ func TestTimecodeSequenceDF(t *testing.T) {
 	}
 }
 
+func TestTcType(t *testing.T) {
+	tests := map[string]struct {
+		TimecodeStr  string
+		ExpectedType timecode.TimeFormat
+	}{
+		"test_1": {TimecodeStr: "00:08:42:03", ExpectedType: timecode.SmpteTimecodeNonDrop},
+		"test_2": {TimecodeStr: "00:14:47:18", ExpectedType: timecode.SmpteTimecodeNonDrop},
+		"test_3": {TimecodeStr: "00:22:23;16", ExpectedType: timecode.SmpteTimecodeDrop},
+		"test_4": {TimecodeStr: "00:29:16;18", ExpectedType: timecode.SmpteTimecodeDrop},
+		"test_5": {TimecodeStr: "01:03:19.345", ExpectedType: timecode.NormalTimestamp},
+		"test_6": {TimecodeStr: "01:09:48.25", ExpectedType: timecode.NormalTimestamp},
+		"test_7": {TimecodeStr: "3456.789", ExpectedType: timecode.FloatSeconds},
+		"test_8": {TimecodeStr: "876.123", ExpectedType: timecode.FloatSeconds},
+		"test_9": {TimecodeStr: "5.678", ExpectedType: timecode.FloatSeconds},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			tcType := timecode.GetTimecodeType(test.TimecodeStr)
+			if tcType != test.ExpectedType {
+				t.Errorf("Expected %s, got %s\n", test.ExpectedType, tcType)
+			}
+		})
+	}
+}
+
+func TestNDFToTc(t *testing.T) {
+	tests := map[string]struct {
+		TimecodeStr string
+		FrameRate   timecode.Rate
+		ExpectedTc  float64
+	}{
+		"test_1":  {TimecodeStr: "00:08:42:03", FrameRate: timecode.Rate_29_97, ExpectedTc: 522.6221}, // 522.6221 = ((8*60*30+42*30+3)*1001/30000)
+		"test_2":  {TimecodeStr: "00:14:47:18", FrameRate: timecode.Rate_29_97, ExpectedTc: 888.4876},
+		"test_3":  {TimecodeStr: "00:22:23:16", FrameRate: timecode.Rate_29_97, ExpectedTc: 1344.8768666666667},
+		"test_4":  {TimecodeStr: "00:29:16:18", FrameRate: timecode.Rate_29_97, ExpectedTc: 1758.3566},
+		"test_5":  {TimecodeStr: "00:37:28:23", FrameRate: timecode.Rate_29_97, ExpectedTc: 2251.0154333333335},
+		"test_6":  {TimecodeStr: "00:44:17:05", FrameRate: timecode.Rate_29_97, ExpectedTc: 2659.8238333333334},
+		"test_7":  {TimecodeStr: "00:50:42:05", FrameRate: timecode.Rate_29_97, ExpectedTc: 3045.208833333333},
+		"test_8":  {TimecodeStr: "00:57:34:19", FrameRate: timecode.Rate_29_97, ExpectedTc: 3458.0879666666665},
+		"test_9":  {TimecodeStr: "01:03:19:21", FrameRate: timecode.Rate_29_97, ExpectedTc: 3803.4997},
+		"test_10": {TimecodeStr: "01:09:48:16", FrameRate: timecode.Rate_29_97, ExpectedTc: 4192.721866666667},
+		"test_11": {TimecodeStr: "01:17:41:10", FrameRate: timecode.Rate_29_97, ExpectedTc: 4665.9946666666665},
+		"test_12": {TimecodeStr: "01:31:32:15", FrameRate: timecode.Rate_29_97, ExpectedTc: 5497.9925},
+		"test_13": {TimecodeStr: "01:39:23:23", FrameRate: timecode.Rate_30, ExpectedTc: 5963.7666},
+		"test_14": {TimecodeStr: "01:47:18:23", FrameRate: timecode.Rate_23_976, ExpectedTc: 6445.397291666667}, //6445.397291666667 = (3600*24+47*60*24+18*24+23)*1001/24000
+		"test_15": {TimecodeStr: "01:47:18:23", FrameRate: timecode.Rate_24, ExpectedTc: 6438.958333333333},     //6438.958333333333 = (3600*24+47*60*24+18*24+23)/24
+		"test_16": {TimecodeStr: "01:17:41:52", FrameRate: timecode.Rate_59_94, ExpectedTc: 4666.528533333333},  //4666.528533333333 = (3600*60+17*60*60+41*60+52)*1001/60000
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			tc, _ := timecode.Parse(test.TimecodeStr, test.FrameRate)
+			tc_seconds := tc.Seconds()
+			if math.Abs(tc_seconds-test.ExpectedTc) > 0.001 {
+				t.Errorf("Expected %f, got %f\n", test.ExpectedTc, tc_seconds)
+			}
+			ndf_timecode, _ := timecode.GetTimeStr(tc_seconds, timecode.SmpteTimecodeNonDrop, test.FrameRate)
+			if ndf_timecode != test.TimecodeStr {
+				t.Errorf("Expected %s, got %s\n", test.TimecodeStr, ndf_timecode)
+			}
+		})
+	}
+
+}
+
 func TestTcConversion(t *testing.T) {
 	// 29.97
 
 	tests := map[string]struct {
 		FrameRate timecode.Rate
 		Sep       string
+		TcFormat  timecode.TimeFormat
 	}{
-		"test_29_97":  {FrameRate: timecode.Rate_29_97, Sep: ";"},
-		"test_59_94":  {FrameRate: timecode.Rate_59_94, Sep: ";"},
-		"test_30":     {FrameRate: timecode.Rate_30, Sep: ":"},
-		"test_23_976": {FrameRate: timecode.Rate_23_976, Sep: ":"},
+		"test_29_97":   {FrameRate: timecode.Rate_29_97, Sep: ";", TcFormat: timecode.SmpteTimecodeDrop},
+		"test_59_94":   {FrameRate: timecode.Rate_59_94, Sep: ";", TcFormat: timecode.SmpteTimecodeDrop},
+		"test_30":      {FrameRate: timecode.Rate_30, Sep: ":", TcFormat: timecode.SmpteTimecodeNonDrop},
+		"test_23_976":  {FrameRate: timecode.Rate_23_976, Sep: ":", TcFormat: timecode.SmpteTimecodeNonDrop},
+		"test_29_97_2": {FrameRate: timecode.Rate_29_97, Sep: ".", TcFormat: timecode.NormalTimestamp},
 	}
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
+			var timecode_str string
 			nominal_fr := test.FrameRate.Nominal
 			frame_num_in_nominal := 0
 			frame_num := 0
 
 			for i := 0; i < nominal_fr*60*60*24+1; i++ {
-				hh := int(frame_num_in_nominal / (3600 * nominal_fr))
-				mm := int((frame_num_in_nominal / (60 * nominal_fr)) % 60)
-				ss := int(((frame_num_in_nominal / nominal_fr) % 60))
-				ff := frame_num_in_nominal % nominal_fr
+				if test.TcFormat == timecode.NormalTimestamp {
+					seconds := float64(frame_num) * float64(test.FrameRate.Den) / float64(test.FrameRate.Num)
 
-				timecode_str := fmt.Sprintf("%02d:%02d:%02d%s%02d", hh, mm, ss, test.Sep, ff)
+					hh := int(seconds) / 3600
+					mm := (int(seconds) / 60) % 60
+					ss := int(seconds) % 60
+					nnn := int(float64(frame_num*test.FrameRate.Den)/float64(test.FrameRate.Num)*1000.) % 1000
+
+					timecode_str = fmt.Sprintf("%02d:%02d:%02d%s%03d", hh, mm, ss, test.Sep, nnn)
+				} else {
+					hh := int(frame_num_in_nominal / (3600 * nominal_fr))
+					mm := int((frame_num_in_nominal / (60 * nominal_fr)) % 60)
+					ss := int(((frame_num_in_nominal / nominal_fr) % 60))
+					ff := frame_num_in_nominal % nominal_fr
+
+					timecode_str = fmt.Sprintf("%02d:%02d:%02d%s%02d", hh, mm, ss, test.Sep, ff)
+				}
 
 				derived_ts, _ := timecode.ParseTimeStr(timecode_str, test.FrameRate)
 
-				derived_tc, _ := timecode.GetTimeStr(derived_ts, timecode.SmpteTimecode, test.FrameRate)
+				derived_tc, _ := timecode.GetTimeStr(derived_ts, test.TcFormat, test.FrameRate)
 
 				//fmt.Println(timecode_str, derived_tc)
 
